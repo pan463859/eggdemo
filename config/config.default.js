0042ddd97a4e0dbfed456145381c4bd506ac88c9
潘小安 <panan521@qq.com>
2022-09-23 19:30:36 +0800

    feat: 完成增删改查接口的 demo,postman 罢工,有空再调试一下

--- config/config.default.js
+++ config/config.default.js
@@ -1,26 +1,55 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1663550954902_752';
+  config.keys = appInfo.name + "_1663550954902_752";
 
   // add your middleware config here
   config.middleware = [];
 
+  config.view = {
+    mapping: { ".html": "ejs" }, //左边写成.html后缀，会自动渲染.html文件
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+    csrf: {
+      enable: false,
+      ignoreJson: true,
+    },
+    dommainWhitelist: ["*"],
+  };
+
+  exports.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: "localhost",
+      // 端口号
+      port: "3306",
+      // 用户名
+      user: "root",
+      // 密码
+      password: "PANan@1995817", // 初始化密码，没设置的可以不写
+      // 数据库名
+      database: "test", // 我们新建的数据库名称
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
   };
 
   return {
--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+  ejs: {
+    enable: true,
+    package: "egg-view-ejs",
+  },
+  mysql: {
+    enable: true,
+    package: 'egg-mysql'
+  }
 };
